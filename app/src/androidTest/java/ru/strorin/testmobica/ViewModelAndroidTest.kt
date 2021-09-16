732be08d17f6359b99fc9f5f2b4bf49d8503fec2
StrorinWind <sakunchik1@yandex.ru>
2021-09-16 18:29:10 +0300

    unit test instrumental test

--- app/src/androidTest/java/ru/strorin/testmobica/ViewModelAndroidTest.kt
+++ app/src/androidTest/java/ru/strorin/testmobica/ViewModelAndroidTest.kt
@@ -2,11 +2,14 @@ package ru.strorin.testmobica
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.ExperimentalSerializationApi
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import ru.strorin.testmobica.network.RetrofitObject
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +17,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ViewModelAndroidTest {
+    @ExperimentalSerializationApi
     @Test
-    fun useAppContext() {
+    fun checkServerConnection() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ru.strorin.testmobica", appContext.packageName)
+
+        val viewModel = ListViewModel(RetrofitObject.retrofitClient, appContext)
+        Thread.sleep(3000) //time to finish request
+        runBlocking {
+            assertTrue(viewModel.data.value?.size ?: 0 > 0)
+        }
     }
 }
